397122b67dfd51f4a57f57a48708d6d7cb6d4da9
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-08-01 17:43:15 -0300

    integration test done

--- back-end/__tests__/integration/recommendationServiceIntegration.test.ts
+++ back-end/__tests__/integration/recommendationServiceIntegration.test.ts
@@ -5,11 +5,9 @@ import app from "../../src/app";
 import { prisma } from "../../src/database";
 
 const endPoint = {
-  home: "/recommendation",
+  base: "/recommendations",
   random: "/random",
   getTop: "/top/:amount",
-  getById: "/:id",
-  upvote: "/:id/upvote",
   downvote: "/:id/downvote",
 };
 
@@ -17,16 +15,82 @@ describe("Insert Router", () => {
   beforeAll(() => {
     execSync("yarn prisma migrate reset --force");
   });
-  it("should insert recommedation", async () => {
-    const response = await supertest(app).post(endPoint.home).send({
+  it("should return 201 (insert recommedation)", async () => {
+    const response = await supertest(app).post(endPoint.base).send({
       name: faker.name.firstName(),
       youtubeLink: "https://www.youtube.com/watch?v=FV-hMoqHtcU",
     });
 
     expect(response.status).toBe(201);
   });
+  it("should return 422 (wrong schema)", async () => {
+    const response = await supertest(app).post(endPoint.base).send({
+      name: faker.name.firstName(),
+      youtubeLink: "https://www.google.com/watch?v=FV-hMoqHtcU",
+    });
+
+    expect(response.status).toBe(422);
+  });
 
   afterAll(() => {
     prisma.$disconnect();
+  });
+});
+
+describe("Upvote Route", () => {
+  it("should return 200 (upvote recommendation)", async () => {
+    const upvoteEndPoint = "/1/upvote";
+    const response = await supertest(app).post(
+      `${endPoint.base}${upvoteEndPoint}`,
+    );
+
+    expect(response.status).toBe(200);
+  });
+  it("should return 404 (not found recommendation to upvote)", async () => {
+    const upvoteEndPoint = "/10/upvote";
+    const response = await supertest(app).post(
+      `${endPoint.base}${upvoteEndPoint}`,
+    );
+
+    expect(response.status).toBe(404);
+  });
+});
+
+describe("Downvote Route", () => {
+  it("should return 200 (downvote recommendation)", async () => {
+    const downvoteEndPoint = "/1/downvote";
+    const response = await supertest(app).post(
+      `${endPoint.base}${downvoteEndPoint}`,
+    );
+
+    expect(response.status).toBe(200);
+  });
+  it("should return 404 (not found recommendation to downvote)", async () => {
+    const downvoteEndPoint = "/10/downvote";
+    const response = await supertest(app).post(
+      `${endPoint.base}${downvoteEndPoint}`,
+    );
+
+    expect(response.status).toBe(404);
+  });
+});
+
+describe("Get by ID Route", () => {
+  it("should return 200 (get by id)", async () => {
+    const getByIdEndPoint = "/1";
+    const response = await supertest(app).get(
+      `${endPoint.base}${getByIdEndPoint}`,
+    );
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should return 404 (not found recommendation id)", async () => {
+    const getByIdEndPoint = "/20";
+    const response = await supertest(app).get(
+      `${endPoint.base}${getByIdEndPoint}`,
+    );
+
+    expect(response.status).toBe(404);
   });
 });
